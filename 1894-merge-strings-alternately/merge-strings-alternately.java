7d431762ff464840dfdcc0a996bb144731320400
Deepshikha Dutta <130208550+Deepshikha07004@users.noreply.github.com>
2025-08-19 20:25:12 +0530

    Time: 6 ms (21.26%) | Memory: 43.2 MB (5.36%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,27 +1,16 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-
-        //calculating the letter count for both strings
-        int l1 = word1.length();
-        int l2 = word2.length();
-        //a string variable to store the merged output
-        String word3 = "";
-        //initializing the loop variable
-        int j = 0;
-        //merging alternate characters till the shorter length among l1 and l2
-        while (j <= (l1 - 1) && j <= (l2 - 1))
+        int len1=word1.length();
+        int len2=word2.length();
+        String merge="";
+        for (int i=0; i<len1 || i<len2; i++)
         {
-            word3 = word3 + word1.charAt(j);
-            word3 = word3 + word2.charAt(j);
-            j++;
+            if (i<len1)
+                merge=merge+word1.charAt(i);
+            if (i<len2)
+                merge=merge+word2.charAt(i);
         }
-        //merging the remaining characters (if any)
-        if (j < l1)
-            word3 = word3 + word1.substring(j);
-        if (j < l2)
-            word3 = word3 + word2.substring(j);
-        
-        return word3;
+        return merge;
         
     }
 }
