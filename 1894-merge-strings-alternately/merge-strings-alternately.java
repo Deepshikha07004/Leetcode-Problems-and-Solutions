5bcca8ab6925f0473b028681cc4af4b06cc877e6
Deepshikha Dutta <130208550+Deepshikha07004@users.noreply.github.com>
2025-08-19 20:26:59 +0530

    Time: 6 ms (21.26%) | Memory: 43.2 MB (5.36%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -3,12 +3,14 @@ class Solution {
         int len1=word1.length();
         int len2=word2.length();
         String merge="";
-        for (int i=0; i<len1 || i<len2; i++)
+        int i=0;
+        while (i<len1 || i<len2)
         {
             if (i<len1)
                 merge=merge+word1.charAt(i);
             if (i<len2)
                 merge=merge+word2.charAt(i);
+            i++;
         }
         return merge;
         
